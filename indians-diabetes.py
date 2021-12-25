347e72aa928111d8b8ae9c09babc458e93d752cd
NicholasBaraldi <nicholas.baraldi@gmail.com>
2021-12-25 19:52:41 -0300

    refactoring and adding accuracy score

--- indians-diabetes.py
+++ indians-diabetes.py
@@ -3,29 +3,32 @@ import pandas as pd
 import sklearn as sk
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
 
 df = pd.read_csv("/home/nicholas/Repos/ML-Project/diabetes.csv")
-print(df.isnull().values.any())
+#print(df.isnull().values.any()) #validating if there is a null row
 
-df = df.append(pd.Series(), ignore_index=True)
+#df = df.append(pd.Series(), ignore_index=True) #adding a null row to test
 
-if df.isnull().values.any():
-    df = df.dropna()
+#if df.isnull().values.any(): #droping the null row
+#    df = df.dropna()
 
-df_train, df_test = train_test_split(df, test_size=0.25, shuffle=False)
+Y = df["Outcome"].values
+
+df_train, df_test, Y_train, Y_test = train_test_split(df, Y, test_size=0.25, shuffle = False)
 
 df_test.drop("Outcome", inplace=True, axis=1)
 
-y = df_train["Outcome"]
-
 features = ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI", "DiabetesPedigreeFunction", "Age"]
 
-X = pd.get_dummies(df_train[features])
+X_train = pd.get_dummies(df_train[features])
 X_test = pd.get_dummies(df_test[features])
 
 model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-model.fit(X, y)
+model.fit(X_train, Y_train)
 predictions = model.predict(X_test)
 
 output = pd.DataFrame({'PersonID': df_test.index, 'Diabetes': predictions})
 output.to_csv('submissionIndian.csv', index=False)
+
+print("{0:.4f}".format(metrics.accuracy_score(Y_test, predictions)))
